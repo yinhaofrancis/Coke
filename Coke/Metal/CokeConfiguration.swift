d4d786bc4569f4ac216ddaa2d6c7f24e1e1b5996
yinhao <yinhao@5eplay.com>
2021-09-29 09:47:30 +0800

    Update CokeConfiguration.swift

--- Coke/Metal/CokeConfiguration.swift
+++ Coke/Metal/CokeConfiguration.swift
@@ -15,8 +15,7 @@ import CoreVideo
 public class CokeMetalConfiguration{
     public var device:MTLDevice
     public var queue:MTLCommandQueue
-    public var sem:DispatchSemaphore = DispatchSemaphore(value: 1)
-    public var threadLimit = DispatchSemaphore(value: 2)
+    public var threadLimit = DispatchSemaphore(value: 1)
     public init() throws{
         let device:MTLDevice? = MTLCreateSystemDefaultDevice()
         guard let dev = device else { throw NSError(domain: "can't create metal context", code: 0, userInfo: nil) }
@@ -32,19 +31,15 @@ public class CokeMetalConfiguration{
     public var shaderLibrary:MTLLibrary!
     
     public func begin() throws ->MTLCommandBuffer {
-        sem.wait()
+        self.threadLimit.wait()
         guard let commandbuffer = self.queue.makeCommandBuffer() else { throw NSError(domain: " can't create command buffer", code: 0, userInfo: nil)}
         commandbuffer.enqueue()
-        sem.signal()
         return commandbuffer
     }
     
     public func commit(buffer:MTLCommandBuffer) {
-        if self.threadLimit.wait(timeout: .now() + .milliseconds(1)) == .success{
-            buffer.commit()
-            buffer.waitUntilCompleted()
-            self.threadLimit.signal()
-        }
+        buffer.commit()
+        self.threadLimit.signal()
     }
     public func function(name:String)->MTLFunction?{
         if let a = self.map[name]{
